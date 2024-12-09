7d9f9e48fb6219077b455329c809c22db01edde0
wada.t00 <wada.t00@DTC23024>
2024-12-09 21:04:22 +0900

    init commit

---
+++ src/test/java/com/example/tasktrackerspring/TaskTrackerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasktrackerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
