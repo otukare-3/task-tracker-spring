984f55f59ecf388a8e217137e2ba39e6e9144d1f
wada.t00 <wada.t00@DTC23024>
2024-12-10 00:54:27 +0900

    テストエラーの修正

--- src/test/java/com/example/tasktrackerspring/application/domain/service/AddTaskServiceTest.java
+++ src/test/java/com/example/tasktrackerspring/application/domain/service/AddTaskServiceTest.java
@@ -14,13 +14,13 @@ class AddTaskServiceTest {
     @Test
     public void addTaskWithStateTodo() {
         AddTaskUseCase sut = new AddTaskService();
-        TaskID taskID = new TaskID(UUID.randomUUID());
-        Description description = new Description("description");
+        TaskID taskID = new TaskID(UUID.fromString("34361936-1cb7-46ef-736d-d175d7218ef7"));
+        Description description = new Description("Test2 Description");
         AddTaskCommand addTaskCommand = new AddTaskCommand(taskID, description, Status.TODO);
 
         sut.add(addTaskCommand);
 
-        LoadTaskPort loadTaskPort = new TaskPersistenceAdapter("");
+        LoadTaskPort loadTaskPort = new TaskPersistenceAdapter("TestData/Test2.json");
         Task loadedTask = loadTaskPort.find(taskID);
         assertEquals(description, loadedTask.description());
     }
@@ -28,13 +28,13 @@ class AddTaskServiceTest {
     @Test
     public void addTaskWithStateTodo_other() {
         AddTaskUseCase sut = new AddTaskService();
-        TaskID taskID = new TaskID(UUID.randomUUID());
-        Description description = new Description("description2");
+        TaskID taskID = new TaskID(UUID.fromString("5b935b5e-e7b7-e2cb-30f3-cc9694b64dfa"));
+        Description description = new Description("Test3 Description");
         AddTaskCommand addTaskCommand = new AddTaskCommand(taskID, description, Status.TODO);
 
         sut.add(addTaskCommand);
 
-        LoadTaskPort loadTaskPort = new TaskPersistenceAdapter("");
+        LoadTaskPort loadTaskPort = new TaskPersistenceAdapter("TestData/Test3.json");
         Task loadedTask = loadTaskPort.find(taskID);
         assertEquals(description, loadedTask.description());
     }
