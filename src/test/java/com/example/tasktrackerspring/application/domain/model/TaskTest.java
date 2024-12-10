fe938f1b7450434d04d8fda45c4bc61ba0a5c35a
wada.t00 <wada.t00@DTC23024>
2024-12-11 06:28:13 +0900

    テスト修正

--- src/test/java/com/example/tasktrackerspring/application/domain/model/TaskTest.java
+++ src/test/java/com/example/tasktrackerspring/application/domain/model/TaskTest.java
@@ -4,16 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class TaskTest {
 
     @Test
     public void canCreate() {
-        Task task = new Task(
-                new TaskID(1),
-                new Description("description"),
-                Status.TODO,
-                LocalDateTime.now(),
-                LocalDateTime.now()
+        assertDoesNotThrow(() -> {
+                    Task task = new Task(
+                            new TaskID(1),
+                            new Description("description"),
+                            Status.TODO,
+                            LocalDateTime.now(),
+                            LocalDateTime.now()
+                    );
+                }
         );
     }
 }
