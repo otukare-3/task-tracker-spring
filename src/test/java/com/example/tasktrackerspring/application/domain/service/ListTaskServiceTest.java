05b331c539fe0c3c3b8e5da447bce3ed15d8e5df
wada.t00 <wada.t00@DTC23024>
2024-12-12 09:02:25 +0900

    todoの追加

---
+++ src/test/java/com/example/tasktrackerspring/application/domain/service/ListTaskServiceTest.java
@@ -0,0 +1,9 @@
+package com.example.tasktrackerspring.application.domain.service;
+
+import com.example.tasktrackerspring.helper.JsonExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(JsonExtension.class)
+public class ListTaskServiceTest {
+    //TODO: ここから
+}
